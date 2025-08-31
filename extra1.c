740e3b8785e06fa48a490469b05de8ad61688fac
Heather Forney <mossbytes@gmail.com>
2025-08-31 15:14:11 -0700

    Update extra1.c
    anonymized submission path

--- extra1.c
+++ extra1.c
@@ -5,7 +5,7 @@ int main (int argc, const char *argv[])
 {
 	char cmd[100];
 
-	char path[] = "/home/faculty/skoss/cse121/h.forney2";
+	char path[] = "/some/faculty/destination/cse121/path";
 
 	sprintf(cmd, "cp %s %s", argv[1], path);
 	system(cmd);
